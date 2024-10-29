bce2805c344a4b3dda0579f9fd9617b862d3ced2
Hangzoed <36575604+Hangzoed@users.noreply.github.com>
2024-10-29 12:10:34 +0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,37 @@
+asmr = ["https://www.youtube.com/watch?v=w9Gi78f6ZfI&t=1186s",
+        "https://www.youtube.com/watch?v=KIb93tPdk3Y",
+        "https://www.youtube.com/watch?v=bmyv3Zy3hlg&list=PL-M5nnffJwq_deyJ2GmHvNqUV78fJmklX&index=8&pp=gAQBiAQB",
+        "https://www.youtube.com/watch?v=9F6ArsO2BIA&list=PL-M5nnffJwq_deyJ2GmHvNqUV78fJmklX&index=1&pp=gAQBiAQB",
+        "https://www.youtube.com/watch?v=QemVt62MS30&list=PL-M5nnffJwq_deyJ2GmHvNqUV78fJmklX&index=11&pp=gAQBiAQB",
+        "https://www.youtube.com/watch?v=fZG3gyVc06Q&list=PL-M5nnffJwq_deyJ2GmHvNqUV78fJmklX&index=15&pp=gAQBiAQB",
+        ]
+
+Bored = ["https://www.youtube.com/watch?v=qu_XQRMWBls",
+         "https://www.youtube.com/watch?v=NJkNN9QFJkc&list=PL-M5nnffJwq-DDVD0RA22MMpcKu62EYbS&index=1&pp=gAQBiAQB8AUB",
+         "https://www.youtube.com/watch?v=APJTcdRwqAU&list=PL-M5nnffJwq-DDVD0RA22MMpcKu62EYbS&index=6&pp=gAQBiAQB8AUB",
+         "https://www.youtube.com/watch?v=_s5AtoaleKs&list=PL-M5nnffJwq-DDVD0RA22MMpcKu62EYbS&index=8&pp=gAQBiAQB8AUB",
+         "https://www.youtube.com/watch?v=b5LlJvrrWrI&list=PL-M5nnffJwq-DDVD0RA22MMpcKu62EYbS&index=2&pp=gAQBiAQB8AUB",
+
+         ]
+
+Relaxed = ["https://www.youtube.com/watch?v=mAHrJGER9XM",
+           "https://www.youtube.com/watch?v=T4QhzaZKPUw&list=PL-M5nnffJwq9grc9fY0a8e03wcnsd5X8c&index=1&pp=gAQBiAQB8AUB",
+           "https://www.youtube.com/watch?v=zKGrm4l62Wc&list=PL-M5nnffJwq9grc9fY0a8e03wcnsd5X8c&index=6&pp=gAQBiAQB8AUB",
+           "https://www.youtube.com/watch?v=7DqDRE_SW34&list=PL-M5nnffJwq9grc9fY0a8e03wcnsd5X8c&index=9&pp=gAQBiAQB8AUB",
+           "https://www.youtube.com/watch?v=0dP7cdf-EgI&list=PL-M5nnffJwq9grc9fY0a8e03wcnsd5X8c&index=7&pp=gAQBiAQB8AUB",
+           "https://www.youtube.com/watch?v=6Y-FMiaNNgQ&list=PL-M5nnffJwq8VhRL1ZrnEl_na1nLKVPOC&index=1&pp=gAQBiAQB8AUB",
+           "https://www.youtube.com/watch?v=3OOcLZ6O2JI&list=PL-M5nnffJwq8VhRL1ZrnEl_na1nLKVPOC&index=8&pp=gAQBiAQB8AUB",
+
+           ]
+
+happy = ["https://www.youtube.com/watch?v=zZdVwTjUtjg",
+"https://www.youtube.com/watch?v=MCW4eGoLX3s",
+"https://www.youtube.com/watch?v=fGKIm9mPY_w&list=RDfGKIm9mPY_w&start_radio=1",
+"https://www.youtube.com/watch?v=zZdVwTjUtjg&list=PL-M5nnffJwq8yUyo42jqJr7GX4ZsqrrPk&index=1&pp=gAQBiAQB8AUB",
+"https://www.youtube.com/watch?v=zhfdsPBiW9Y&list=PL-M5nnffJwq8yUyo42jqJr7GX4ZsqrrPk&index=9&pp=gAQBiAQB8AUB",
+"https://www.youtube.com/watch?v=dzH9mYS8RDY&list=PL-M5nnffJwq8yUyo42jqJr7GX4ZsqrrPk&index=10&pp=gAQBiAQB8AUB",
+"https://www.youtube.com/watch?v=9kD4tRoTqhs&list=PL-M5nnffJwq8yUyo42jqJr7GX4ZsqrrPk&index=5&pp=gAQBiAQB8AUB",
+"https://www.youtube.com/watch?v=fGKIm9mPY_w&list=PL-M5nnffJwq8yUyo42jqJr7GX4ZsqrrPk&index=2&pp=gAQBiAQB8AUB",
+
+
+]
